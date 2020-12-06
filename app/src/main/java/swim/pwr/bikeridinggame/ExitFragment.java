c8376fd785d4530052f12742f6306b411936a2e8
ukasz09 <246703@student.pwr.edu.pl>
2020-12-06 23:43:41 +0100

    Fragments cleanup

--- app/src/main/java/swim/pwr/bikeridinggame/ExitFragment.java
+++ app/src/main/java/swim/pwr/bikeridinggame/ExitFragment.java
@@ -10,28 +10,10 @@ import android.view.ViewGroup;
 
 import java.util.Objects;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExitFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ExitFragment extends Fragment {
 
     public ExitFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment ExitFragment.
-     */
-    public static ExitFragment newInstance() {
-        ExitFragment fragment = new ExitFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
